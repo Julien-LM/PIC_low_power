0609ac59ce567d66922a98c51725c5cfdde23c16
Julien-LM <julien.lemellec@gmail.com>
2018-02-01 17:15:24 +0100

    [file]: create main file

---
+++ PIC_low_power.X/main.c
@@ -0,0 +1,18 @@
+/*
+ * File:   main.c
+ * Author: ju
+ *
+ * Created on February 1, 2018, 5:12 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    while(1) {
+        NOP();
+    }
+    mlk
+    
+    return;
+}
