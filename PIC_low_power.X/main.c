c42ee314a9bc21dffb8d1c5971856296d99d4162
Julien-LM <julien.lemellec@gmail.com>
2018-02-16 14:40:44 +0100

    [I/O]: config every pin as output
    	- Set them to low state
    	- Consumption is no longer depending external environment

--- PIC_low_power.X/main.c
+++ PIC_low_power.X/main.c
@@ -11,8 +11,20 @@
 
 void main(void) {
     
-    OSCCONbits.IRCF = 0b1111; //4 Mhz
+    OSCCONbits.IRCF = 0b0111; //4 Mhz
     
+    ANSELA = 0x00;
+    ANSELB = 0x00;
+    ANSELC = 0x00;
+    
+    TRISA = 0x00;
+    TRISB = 0x00;
+    TRISC = 0x00;
+    
+    PORTA = 0x00;
+    PORTB = 0x00;
+    PORTC = 0x00;
+
     while(1) {
         NOP();
     }
